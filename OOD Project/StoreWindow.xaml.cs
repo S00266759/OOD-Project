a13e5fb90dc114a63a63e708228acdf92d316f46
Hannah Rodtmann <S00266759@atu.ie>
2026-03-11 15:04:29 +0000

    updating games list for store window (added 4+ games, more to be implemented)

--- OOD Project/StoreWindow.xaml.cs
+++ OOD Project/StoreWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Data;
 using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 
 namespace OOD_Project
@@ -29,6 +32,11 @@ namespace OOD_Project
             Games.Add(new Game { Title = "CyberGame 2077", Genre = "RPG", Platform = "PC", Description = "Futuristic RPG action game." });
             Games.Add(new Game { Title = "Elden Ring", Genre = "Action", Platform = "PC", Description = "Epic open world fantasy adventure." });
             Games.Add(new Game { Title = "Halo Infinite", Genre = "Shooter", Platform = "Xbox", Description = "Classic sci-fi shooter experience." });
+            Games.Add(new Game { Title = "Doom Eternal", Genre = "Shooter", Platform = "Xbox", Description = "Classic shooter experience." });
+            Games.Add(new Game { Title = "The Legend of Zelda: Breath of the Wild", Genre = "Adventure", Platform = "Nintendo Switch", Description = "Open world adventure that battles the evil overlord, Ganondorf and his reign of darkness over Hyrule" });
+            Games.Add(new Game { Title = "The Legend of Zelda: Ocarina of Time", Genre = "Adventure", Platform = "Nintendo 64", Description = "Open world adventure that battles the evil overlord, Ganondorf and his reign of darkness over Hyrule" });
+            Games.Add(new Game { Title = "The Legend of Zelda: Majora's Mask", Genre = "Adventure", Platform = "Nintendo 64", Description = "Open world adventure that battles the evil overlord, Ganondorf and his reign of darkness over Hyrule" });
+
 
             // Binding to the ListBox
             lstGames.ItemsSource = Games;
