2a130778f0b9c92bc795bd8d1049ab32868169d9
Hannah Rodtmann <S00266759@atu.ie>
2026-03-18 09:58:13 +0000

    Added a purchase class to be replaced by database later

---
+++ OOD Project/classes(database later)/Purchase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_Project.classes_database_later_
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string GameTitle { get; set; }
+        public DateTime PurchaseDate { get; set; }
+    }
+}
