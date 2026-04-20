42812abb8d6ce655c84010b6d3e671e717d40135
Hannah Rodtmann <S00266759@atu.ie>
2026-04-20 20:49:40 +0100

    Updated Account + Rewards window. (changed UI/Styling, added a points progress bar, added a rewards selection combobox where user can choose which reward to redeem given amount of points)

--- OOD Project/Account+Rewards.xaml.cs
+++ OOD Project/Account+Rewards.xaml.cs
@@ -3,6 +3,8 @@ using OOD_Project.Data;
 using OOD_Project.ProjectData;
 using System.Linq;
 using System;
+using System.Net.NetworkInformation;
+using System.Windows.Controls;
 
 namespace OOD_Project
 {
@@ -34,6 +36,8 @@ namespace OOD_Project
 
                 tblkUsername.Text = CurrentUser.Username;
                 tblkPoints.Text = CurrentUser.Points.ToString();
+
+                prgPoints.Value = CurrentUser.Points;
             }
         }
 
@@ -45,26 +49,91 @@ namespace OOD_Project
                 {
                     var user = context.Users.FirstOrDefault();
 
-                    if (user != null)
+                    if (user != null && cbxRewards.SelectedItem != null)
                     {
-                        if (user.Points >= 500)
-                        {
-                            user.Points -= 500;
-                            context.SaveChanges();
+                        string selectedReward = (cbxRewards.SelectedItem as ComboBoxItem).Content.ToString();
 
+                        int cost = 0;
+
+                        if (selectedReward.Contains("500")) cost = 500;
+                        else if (selectedReward.Contains("1000")) cost = 1000;
+                        else if (selectedReward.Contains("3500")) cost = 3500;
+                        else if (selectedReward.Contains("5000")) cost = 5000;
+
+                        if (user.Points >= cost)
+                        {
+                            user.Points -= cost;
+                            context.SaveChanges();
+                            
                             tblkPoints.Text = user.Points.ToString();
-                            MessageBox.Show("Reward Redeemed!");
+                            prgPoints.Value = CurrentUser.Points;
+                            MessageBox.Show( selectedReward + "Redeemed! Enjoy!");
                         }
                         else
                         {
                             MessageBox.Show("Not enough points.");
                         }
                     }
+                    else
+                    {
+                        MessageBox.Show("Please select a reward.");
+                    }
                 }
+                
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error redeeming reward: " + ex.Message);
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new GamezExpressContext())
+                {
+                    var user = context.Users.FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        user.Points = 0;
+                        context.SaveChanges();
+
+                        tblkPoints.Text = user.Points.ToString();
+
+                        MessageBox.Show("Points reset for testing.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error resetting points: " + ex.Message);
+            }
+        }
+
+        private void cbxRewards_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbxRewards.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string reward = selectedItem.Content.ToString();
+
+                if (reward.Contains("10%"))
+                {
+                    tblkRewardPreview.Text = "Get 10% off your next game purchase.";
+                }
+                else if (reward.Contains("20%"))
+                {
+                    tblkRewardPreview.Text = "Get 20% off your next game purchase.";
+                }
+                else if (reward.Contains("50%"))
+                {
+                    tblkRewardPreview.Text = "Massive 50% discount on any game!";
+                }
+                else if (reward.Contains("Free"))
+                {
+                    tblkRewardPreview.Text = "Unlock a random free game from the store!";
+                }
             }
         }
     }
